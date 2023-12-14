75e8e5e393d47d6f808272fc2bb8fd82bf8a999b
Ph Minh Dong <142565609+dongdev06@users.noreply.github.com>
2023-12-14 09:03:35 +0700

    Update index.js

--- Extra/Database/index.js
+++ Extra/Database/index.js
@@ -7,11 +7,10 @@ const fs = require('fs-extra');
 const request = require('request');
 const deasync = require('deasync');
 
-if (!fs.existsSync(process.cwd() + '/Orion_Database')) {
-    fs.mkdirSync(process.cwd() + '/Orion_Database');
-    fs.writeFileSync(process.cwd() + '/Orion_Database/A_README.md', 'This folder is used by ChernobyL(NANI =)) ) to store data. Do not delete this folder or any of the files in it.', 'utf8');
+if (!fs.existsSync(process.cwd() + '/Dongdev_Database')) {
+    fs.mkdirSync(process.cwd() + '/Dongdev_Database');
 }
-var db = new BetterDB(process.cwd() + "/Orion_Database/SyntheticDatabase.sqlite");
+var db = new BetterDB(process.cwd() + "/Dongdev_Database/SyntheticDatabase.sqlite");
 
 function Lset(key, value) {
     if (!key)
