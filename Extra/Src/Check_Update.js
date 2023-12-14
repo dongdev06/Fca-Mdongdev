17be66f69e986fe2ca18d8895abd92a58a37a3f1
Ph Minh Dong <142565609+dongdev06@users.noreply.github.com>
2023-12-14 09:01:48 +0700

    Update Check_Update.js

--- Extra/Src/Check_Update.js
+++ Extra/Src/Check_Update.js
@@ -4,16 +4,16 @@ module.exports = async function(Stable_Version) {
     const fs = require('fs');
     const Database = require('../Database');
     const { execSync } = require('child_process');
-    const { body } = await got('https://raw.githubusercontent.com/ivancotacte/fca-project-orion/main/package.json');
+    const { body } = await got('https://raw.githubusercontent.com/dongdev06/Fca-Mdongdev/main/package.json');
     const json = JSON.parse(body);
     const LocalVersion = require('../../package.json').version;
         if (Number(LocalVersion.replace(/\./g,"")) < Number(json.version.replace(/\./g,"")) && global.Fca.Require.FastConfig.Stable_Version.Accept == false || Stable_Version && Number(LocalVersion.replace(/\./g,"")) != Number(Stable_Version.replace(/\./g,""))) {
             var Version = Stable_Version != undefined ? Stable_Version : json.version;
-            log.warn("[ FCA-UPDATE ] •","New Version, Ready to Update: " + LocalVersion + " -> " + Version);    
+            log.warn("〈 FCA-UPDATE 〉➜","New Version, Ready to Update: " + LocalVersion + " -> " + Version);    
             await new Promise(resolve => setTimeout(resolve, 3000));
             try {
-                execSync(`npm install fca-project-orion@${Version}`, { stdio: 'inherit' });
-                log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
+                execSync(`npm install fca-mdongdev@${Version}`, { stdio: 'inherit' });
+                log.info("〈 FCA-UPDATE 〉➜","Update Complete, Restarting...");
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 Database().set("Instant_Update", Date.now());
                 await new Promise(resolve => setTimeout(resolve, 3000));
@@ -22,10 +22,10 @@ module.exports = async function(Stable_Version) {
             catch (err) {
                 try {
                     console.log(err);  
-                    log.warn("[ FCA-UPDATE ] •","Update Failed, Trying Another Method 1...");
+                    log.warn("〈 FCA-UPDATE 〉➜","Update Failed, Trying Another Method 1...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
-                    execSync(`npm install fca-project-orion@${Version} --force`, { stdio: 'inherit' });
-                    log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
+                    execSync(`npm install fca-mdongdev@${Version} --force`, { stdio: 'inherit' });
+                    log.info("〈 FCA-UPDATE 〉➜","Update Complete, Restarting...");
                     await new Promise(resolve => setTimeout(resolve, 3000));
                     Database().set("Instant_Update", Date.now());
                     await new Promise(resolve => setTimeout(resolve, 3000));
@@ -34,16 +34,16 @@ module.exports = async function(Stable_Version) {
                 catch (err) {
                     try {
                         console.log(err);
-                        log.warn("[ FCA-UPDATE ] •","Update Failed, Trying to clean Database() cache...");
+                        log.warn("〈 FCA-UPDATE 〉➜","Update Failed, Trying to clean Database() cache...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         execSync('npm cache clean --force', { stdio: 'inherit' });
-                        log.info("[ FCA-UPDATE ] •","Cache Cleaned, Trying Another Method 2...");
+                        log.info("〈 FCA-UPDATE 〉➜","Cache Cleaned, Trying Another Method 2...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         //self delete fca-horizon-remastered folder from node_modules
-                        fs.rmdirSync((process.cwd() + "/node_modules/fca-project-orion" || __dirname + '../../../fca-project-orion'), { recursive: true });
+                        fs.rmdirSync((process.cwd() + "/node_modules/fca-mdongdev" || __dirname + '../../../fca-mdongdev'), { recursive: true });
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        execSync(`npm install fca-project-orion@${Version}`, { stdio: 'inherit' });
-                        log.info("[ FCA-UPDATE ] •","Update Complete, Restarting...");
+                        execSync(`npm install fca-mdongdev@${Version}`, { stdio: 'inherit' });
+                        log.info("〈 FCA-UPDATE 〉➜","Update Complete, Restarting...");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         Database().set("Instant_Update", Date.now(), true);
                         await new Promise(resolve => setTimeout(resolve, 3000));
@@ -51,9 +51,9 @@ module.exports = async function(Stable_Version) {
                     }
                     catch (e) {
                         console.log(e);
-                        log.error("[ FCA-UPDATE ] •","Update Failed, Please Update Manually");
+                        log.error("〈 FCA-UPDATE 〉➜","Update Failed, Please Update Manually");
                         await new Promise(resolve => setTimeout(resolve, 3000));
-                        log.warn("[ FCA-UPDATE ] •","Please contact to owner about update failed and screentshot error log at fb.com/Lazic.Kanzu");
+                        log.warn("〈 FCA-UPDATE 〉➜","Please contact to owner about update failed and screentshot error log at Fb.com/Donqdz.profile.206");
                         await new Promise(resolve => setTimeout(resolve, 3000));
                         process.exit(1);
                     }
